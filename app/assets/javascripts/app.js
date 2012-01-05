76caaa715b146a7201ee513c2f33513df04005e3
Aleksey Gureiev <spyromus@noizeramp.com>
2012-01-05 21:28:10 +0200

    Added initial comments section

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -56,7 +56,51 @@ Coliseum.Player = Ember.Object.create({
     return this.video && "http://www.youtube.com/embed/" + this.video.id;
   }.property('video'),
 
-  play: function(v) {
-    this.video = v;
+  isVideoSet: function() {
+    return !!this.video;
+  }.property('video')
+});
+
+Coliseum.Comment = Ember.Object.extend({
+  body: null
+});
+
+Coliseum.commentsList = Ember.ArrayController.create({
+  content: [],
+  current_vid: null,
+  tempStorage: {},
+  comment: null,
+
+  refresh: function() {
+    var vid = Coliseum.Player.video.id;
+
+    if (this.current_vid) {
+      this.tempStorage[this.current_vid] = this.get('content').map(function(i) {
+        return i.get('body'); });
+    }
+
+    var c = (this.tempStorage[vid] || []).map(function(body) {
+      return Coliseum.Comment.create({ body: body }); });
+
+    this.set('content', c);
+
+    this.current_vid = vid;
+    this.set('comment', '');
+
+    setTimeout(function() {
+      $("#player").show();
+      $("#player-note").hide();
+    }, 500);
+  }.observes('Coliseum.Player.video'),
+
+  addComment: function() {
+    this.pushObject(Coliseum.Comment.create({ body: this.comment }));
+    this.set('comment', '');
+  }
+});
+
+Coliseum.NewCommentView = Ember.TextField.extend({
+  insertNewline: function() {
+    Coliseum.commentsList.addComment();
   }
 });
